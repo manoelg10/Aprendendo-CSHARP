e5fc09e85748153c2f8cacaacede24558da903ec
manoelg10 <137456949+manoelg10@users.noreply.github.com>
2024-03-13 19:29:39 -0300

    Update Program.cs

--- loteria/Program.cs
+++ loteria/Program.cs
@@ -10,8 +10,10 @@ namespace loteria
     {
         static void Main(string[] args)
         {
-            double ganhoTotal, apostaPrimeiro, apostaSegundo, apostaTerceiro, apostaTotal, ganhoPrimeiro, ganhoSegundo, ganhoTerceiro;
 
+            double ganhoTotal, apostaPrimeiro, apostaSegundo, apostaTerceiro, apostaTotal, ganhoPrimeiro, ganhoSegundo, ganhoTerceiro;
+            Console.WriteLine("----Bem-Vindo ao programa Loteria Premiada---");
+            Console.WriteLine("--------------------------------------------");
             Console.WriteLine("Insira o valor total do prêmio (R$): ");
             ganhoTotal = double.Parse(Console.ReadLine());
             Console.WriteLine("Insira o valor da primeira aposta (R$): ");
@@ -26,10 +28,13 @@ namespace loteria
             ganhoSegundo = (apostaSegundo / apostaTotal) * ganhoTotal;
             ganhoTerceiro = (apostaTerceiro / apostaTotal) * ganhoTotal;
 
-            Console.WriteLine($"O valor do prêmio é: {ganhoTotal}");
-            Console.WriteLine($"O valor apostado pelo primeiro apostador é: {apostaPrimeiro} R$ e portanto o valor do prêmio para ele é: {ganhoPrimeiro} R$");
-            Console.WriteLine($"O valor apostado pelo segundo apostador é: {apostaSegundo} R$ e portanto o valor do prêmio para ele é: {ganhoSegundo} R$");
-            Console.WriteLine($"O valor apostado pelo terceiro apostador é: {apostaTerceiro} R$ e portanto o valor do prêmio para ele é: {ganhoTerceiro} R$");
+            Console.WriteLine($"O valor do prêmio é de: {ganhoTotal} R$");
+            Console.WriteLine("Após o resultado, a divisão dos valores entre os amigos se deu da seguinte maneira: ");
+            Console.WriteLine($"O valor apostado pelo primeiro apostador é: {apostaPrimeiro} R$ e portanto o valor do prêmio para ele é: {ganhoPrimeiro:0.00} R$");
+            Console.WriteLine($"O valor apostado pelo segundo apostador é: {apostaSegundo} R$ e portanto o valor do prêmio para ele é: {ganhoSegundo:0.00} R$");
+            Console.WriteLine($"O valor apostado pelo terceiro apostador é: {apostaTerceiro} R$ e portanto o valor do prêmio para ele é: {ganhoTerceiro:0.00} R$");
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Tecle ENTER para encerrar o programa");
             Console.ReadLine();
         }
     }
